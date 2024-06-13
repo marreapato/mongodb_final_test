f40d11c184b9b8da978343c0da346b249532558e
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-06-13 20:48:35 -0300

    Create jogos.js

---
+++ teste_final/jogos.js
@@ -0,0 +1,282 @@
+
+
+
+db.jogosVideoGame.insertMany([
+     {
+        "titulo": "The Last of Us",
+        "genero": "Ação e Aventura",
+        "idiomasDisponiveis": ["Inglês", "Português", "Espanhol"],
+        "anoLancamento": 2013,
+        "criticas": [
+            {"site": "Metacritic", "avaliacao": 9.7},
+            {"site": "IGN", "avaliacao": 10}
+        ],
+        "quantidadeHoras": 170,
+        "preco": 59.99,
+        "classificacaoIndicativa": "18+"
+    },
+    {
+        "titulo": "The Last of Us Part II",
+        "genero": "Ação e Aventura",
+        "idiomasDisponiveis": ["Inglês", "Português", "Espanhol"],
+        "anoLancamento": 2020,
+        "criticas": [
+            {"site": "Metacritic", "avaliacao": 9.5},
+            {"site": "IGN", "avaliacao": 9.3}
+        ],
+        "quantidadeHoras": 40,
+        "preco": 69.99,
+        "classificacaoIndicativa": "18+"
+    },
+    {
+        "titulo": "Grand Theft Auto V",
+        "genero": "Ação",
+        "idiomasDisponiveis": ["Inglês", "Português", "Espanhol", "Francês", "Alemão"],
+        "anoLancamento": 2013,
+        "criticas": [
+            {"site": "Metacritic", "avaliacao": 9.5},
+            {"site": "IGN", "avaliacao": 10},
+            {"site": "GameSpot", "avaliacao": 9.6}
+        ],
+        "quantidadeHoras": 110,
+        "preco": 29.99,
+        "classificacaoIndicativa": "18+"
+    },
+    {
+        "titulo": "Grand Theft Auto IV",
+        "genero": "Ação",
+        "idiomasDisponiveis": ["Inglês", "Português", "Espanhol", "Francês", "Alemão"],
+        "anoLancamento": 2008,
+        "criticas": [
+            {"site": "Metacritic", "avaliacao": 9.0},
+            {"site": "IGN", "avaliacao": 9.2},
+            {"site": "GameSpot", "avaliacao": 9.4}
+        ],
+        "quantidadeHoras": 25,
+        "preco": 19.99,
+        "classificacaoIndicativa": "18+"
+    },
+    {
+        "titulo": "Red Dead Redemption 2",
+        "genero": "Ação e Aventura",
+        "idiomasDisponiveis": ["Inglês", "Português", "Espanhol", "Francês", "Alemão"],
+        "anoLancamento": 2018,
+        "criticas": [
+            {"site": "Metacritic", "avaliacao": 9.8},
+            {"site": "IGN", "avaliacao": 10},
+            {"site": "GameSpot", "avaliacao": 9.7}
+        ],
+        "quantidadeHoras": 430,
+        "preco": 59.99,
+        "classificacaoIndicativa": "18+"
+    },
+    {
+        "titulo": "The Witcher 3: Wild Hunt",
+        "genero": "RPG",
+        "idiomasDisponiveis": ["Inglês", "Português", "Espanhol", "Polonês", "Francês"],
+        "anoLancamento": 2015,
+        "criticas": [
+            {"site": "Metacritic", "avaliacao": 9.5},
+            {"site": "IGN", "avaliacao": 9.3},
+            {"site": "GameSpot", "avaliacao": 9.6}
+        ],
+        "quantidadeHoras": 3000,
+        "preco": 39.99,
+        "classificacaoIndicativa": "18+"
+    },
+    {
+        "titulo": "Half-Life 2",
+        "genero": "FPS",
+        "idiomasDisponiveis": ["Inglês", "Português", "Espanhol"],
+        "anoLancamento": 2004,
+        "criticas": [
+            {"site": "Metacritic", "avaliacao": 9.6},
+            {"site": "IGN", "avaliacao": 9.8}
+        ],
+        "quantidadeHoras": 120,
+        "preco": 9.99,
+        "classificacaoIndicativa": "16+"
+    },
+    {
+        "titulo": "Portal 2",
+        "genero": "Puzzle",
+        "idiomasDisponiveis": ["Inglês", "Português", "Espanhol", "Francês", "Alemão"],
+        "anoLancamento": 2011,
+        "criticas": [
+            {"site": "Metacritic", "avaliacao": 9.5},
+            {"site": "IGN", "avaliacao": 9.4},
+            {"site": "GameSpot", "avaliacao": 9.6}
+        ],
+        "quantidadeHoras": 40,
+        "preco": 19.99,
+        "classificacaoIndicativa": "10+"
+    },
+    {
+        "titulo": "The Elder Scrolls V: Skyrim",
+        "genero": "RPG",
+        "idiomasDisponiveis": ["Inglês", "Português", "Espanhol", "Francês", "Alemão"],
+        "anoLancamento": 2011,
+        "criticas": [
+            {"site": "Metacritic", "avaliacao": 9.3},
+            {"site": "IGN", "avaliacao": 9.1},
+            {"site": "GameSpot", "avaliacao": 9.2}
+        ],
+        "quantidadeHoras": 600,
+        "preco": 29.99,
+        "classificacaoIndicativa": "16+"
+    },
+    {
+        "titulo": "The Elder Scrolls IV: Morrowind",
+        "genero": "RPG",
+        "idiomasDisponiveis": ["Inglês", "Português"],
+        "anoLancamento": 2002,
+        "criticas": [
+            {"site": "Metacritic", "avaliacao": 9.1},
+            {"site": "IGN", "avaliacao": 8.9}
+        ],
+        "quantidadeHoras": 400,
+        "preco": 9.99,
+        "classificacaoIndicativa": "16+"
+    },
+    {
+        "titulo": "The Elder Scrolls III: Oblivion",
+        "genero": "RPG",
+        "idiomasDisponiveis": ["Inglês", "Português", "Espanhol", "Francês"],
+        "anoLancamento": 2006,
+        "criticas": [
+            {"site": "Metacritic", "avaliacao": 9.2},
+            {"site": "IGN", "avaliacao": 9.0},
+            {"site": "GameSpot", "avaliacao": 8.8}
+        ],
+        "quantidadeHoras": 102,
+        "preco": 14.99,
+        "classificacaoIndicativa": "16+"
+    },
+    {
+        "titulo": "The Elder Scrolls II: Daggerfall",
+        "genero": "RPG",
+        "idiomasDisponiveis": ["Inglês"],
+        "anoLancamento": 1996,
+        "criticas": [
+            {"site": "Metacritic", "avaliacao": 8.5},
+            {"site": "IGN", "avaliacao": 8.7}
+        ],
+        "quantidadeHoras": 75,
+        "preco": 4.99,
+        "classificacaoIndicativa": "14+"
+    },
+    {
+        "titulo": "The Elder Scrolls",
+        "genero": "RPG",
+        "idiomasDisponiveis": ["Inglês"],
+        "anoLancamento": 1994,
+        "criticas": [
+            {"site": "Metacritic", "avaliacao": 7.8},
+            {"site": "IGN", "avaliacao": 7.5}
+        ],
+        "quantidadeHoras": 30,
+        "preco": 2.99,
+        "classificacaoIndicativa": "14+"
+    },
+    {
+        "titulo": "The Legend of Zelda: Breath of the Wild",
+        "genero": "Ação e Aventura",
+        "idiomasDisponiveis": ["Inglês", "Português", "Espanhol", "Japonês"],
+        "anoLancamento": 2017,
+        "criticas": [
+            {"site": "Metacritic", "avaliacao": 9.7},
+            {"site": "IGN", "avaliacao": 10}
+        ],
+        "quantidadeHoras": 200,
+        "preco": 59.99,
+        "classificacaoIndicativa": "10+"
+    },
+    {
+        "titulo": "God of War",
+        "genero": "Ação e Aventura",
+        "idiomasDisponiveis": ["Inglês", "Português", "Espanhol"],
+        "anoLancamento": 2018,
+        "criticas": [
+            {"site": "Metacritic", "avaliacao": 9.6},
+            {"site": "IGN", "avaliacao": 10}
+        ],
+        "quantidadeHoras": 60,
+        "preco": 39.99,
+        "classificacaoIndicativa": "18+"
+    },
+    {
+        "titulo": "Cyberpunk 2077",
+        "genero": "RPG",
+        "idiomasDisponiveis": ["Inglês", "Português", "Espanhol", "Francês", "Alemão"],
+        "anoLancamento": 2020,
+        "criticas": [
+            {"site": "Metacritic", "avaliacao": 8.5},
+            {"site": "IGN", "avaliacao": 9.0}
+        ],
+        "quantidadeHoras": 150,
+        "preco": 49.99,
+        "classificacaoIndicativa": "18+"
+    },
+    {
+        "titulo": "Super Mario Odyssey",
+        "genero": "Plataforma",
+        "idiomasDisponiveis": ["Inglês", "Português", "Espanhol", "Francês", "Alemão", "Japonês"],
+        "anoLancamento": 2017,
+        "criticas": [
+            {"site": "Metacritic", "avaliacao": 9.6},
+            {"site": "IGN", "avaliacao": 10}
+        ],
+        "quantidadeHoras": 50,
+        "preco": 59.99,
+        "classificacaoIndicativa": "L"
+    },
+    {
+        "titulo": "Minecraft",
+        "genero": "Sandbox",
+        "idiomasDisponiveis": ["Inglês", "Português", "Espanhol", "Francês", "Alemão"],
+        "anoLancamento": 2011,
+        "criticas": [
+            {"site": "Metacritic", "avaliacao": 9.3},
+            {"site": "IGN", "avaliacao": 9.0}
+        ],
+        "quantidadeHoras": 9999,
+        "preco": 26.95,
+        "classificacaoIndicativa": "L"
+    },
+    {
+        "titulo": "Among Us",
+        "genero": "Multijogador",
+        "idiomasDisponiveis": ["Inglês", "Português", "Espanhol", "Francês", "Alemão"],
+        "anoLancamento": 2018,
+        "criticas": [
+            {"site": "Metacritic", "avaliacao": 8.0},
+            {"site": "IGN", "avaliacao": 8.5}
+        ],
+        "quantidadeHoras": 100,
+        "preco": 4.99,
+        "classificacaoIndicativa": "10+"
+    }
+]);
+
+
+
+db.jogosVideoGame.save({
+    "_id": ObjectId("6669d860e4b7044b12c9eb5e"),
+    "titulo": "Sonic",
+    "genero": "Plataforma",
+    "idiomasDisponiveis": ["Inglês", "Português", "Espanhol"],
+    "anoLancamento": 1991,
+    "criticas": [
+        {"site": "Metacritic", "avaliacao": 8.8},
+        {"site": "IGN", "avaliacao": 8.5}
+    ],
+    "quantidadeHoras": 20,
+    "preco": 14.99,
+    "classificacaoIndicativa": "L"
+});
+
+
+db.jogosVideoGame.updateOne(
+    { titulo: "The Witcher 3: Wild Hunt" },
+    { $set: { quantidadeHoras: 200 } }
+);
